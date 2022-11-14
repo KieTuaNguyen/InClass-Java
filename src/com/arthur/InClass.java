bb1f24751333282fb379f464d3bae0dd2ecd3a1b
KieTuaNguyen <kietntgdd210002@fpt.edu.vn>
2022-11-14 17:13:37 +0700

    1st commit

---
+++ src/com/arthur/InClass.java
@@ -0,0 +1,27 @@
+package com.arthur;
+
+import java.util.Scanner;
+
+public class InClass {
+    public static void main(String[] args) {
+        System.out.println("Xin chao the gioi!");
+
+        // Read and display a string
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scnr.nextLine();
+        System.out.println("Enter your age: ");
+        String age = scnr.nextLine();
+        System.out.println("Enter your address: ");
+        String address = scnr.nextLine();
+        System.out.println("Hello ban " + name + " " + age + " tuoi, o " + address + " ;)");
+
+        System.out.println("Enter your previous grade: ");
+        float grade = scnr.nextFloat();
+        if (grade >= 5) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
